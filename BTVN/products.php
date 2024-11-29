f9188dfe288cbcb4cb8e222511f9f7bac9dabdb8
TuanLinh84 <151935366+TuanLinh84@users.noreply.github.com>
2024-11-30 02:16:24 +0700

    Add files via upload

---
+++ BTVN/products.php
@@ -0,0 +1,19 @@
+<?php
+$products = array (
+  0 => 
+  array (
+    'name' => 'Sản phẩm 1',
+    'price' => '10000',
+  ),
+  1 => 
+  array (
+    'name' => 'Sản phẩm 2',
+    'price' => '2000',
+  ),
+  2 => 
+  array (
+    'name' => 'Sản phẩm 3',
+    'price' => '3000',
+  ),
+);
+?>
